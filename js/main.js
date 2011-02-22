63e564dc8ff12a9623bbc2d7dbe5948b161c4b74
dha <dha@bluemars.net>
2011-02-22 17:15:14 +0100

    Einstellungen können gespeichert, überschrieben und gelöscht werden. Weiterleitungen zwischen Seiten sind möglich

--- js/main.js
+++ js/main.js
@@ -31,17 +31,46 @@ var trackr = {
     
     init: function() {
 
+        trackr.populateFormFields();
+        
         $('#settings_form .submit').click(function() {
             trackr.saveSettings();
-        })
+        });
+        
+        $('#delete_settings .submit').click(function() {
+            trackr.deleteData();
+        });
     
+    },
+    
+    /**
+     * Populate Forms with Values from local Storage
+     *
+     */
+    populateFormFields: function() {
+    
+        // PLZ
+        if(localStorage.getItem('plz')) {
+            $('#settings_form #plz').val(localStorage.getItem('plz'));
+        }
+        
+        // Sparten
+        if(localStorage.getItem('sparten')) {
+            var sparten = localStorage.getItem('sparten').split(',');
+             $('#settings_form input:checkbox').each(function() {
+                if ($.inArray($(this).attr('name'), sparten) >= 0) {
+                    $(this).attr('checked', true);                   
+                }
+            });
+        } 
+                
     },
     
     /**
      * Save settings to local Storage
      *
      */
-    saveSettings: function(form) {
+    saveSettings: function() {
     
         // Save PLZ
         localStorage.setItem('plz', $('#settings_form #plz').val());
@@ -53,7 +82,35 @@ var trackr = {
         });
         localStorage.setItem('sparten',sparten);       
     
-    }
+    },
+    
+    /**
+     * Delete all Settings, clear local Storage
+     *
+     */
+    deleteData: function() {
+        
+        var deleted = false;
+        
+        if ($('#delete_settings input[name="einstellungen"]').attr('checked')) {
+            localStorage.removeItem('plz');
+            localStorage.removeItem('sparten');
+            $('#settings_form input:checkbox').each(function() { $(this).attr('checked', false) });
+            console.log('PLZ/Sparten gelöscht');
+            deleted = true;
+        };
+        if ($('#delete_settings input[name="messwerte"]').attr('checked')) {
+            localStorage.removeItem('messwerte');
+            $('#settings_form #plz').val('PLZ');
+            console.log('Messwerte gelöscht');            
+            deleted = true;
+        };
 
+        if (deleted) {
+            alert("Lokale Daten gelöscht.");
+            window.location.hash = '#settings';
+        }
+    
+    }
 
 }
